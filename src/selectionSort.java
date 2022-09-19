b102caecd3b97dc8b9c180ba939f7c5302789e30
KARTIKEY SOKHAL <KARTIKEY SOKHAL@10.12.39.30>
2022-09-19 10:26:22 +0530

    again

--- src/selectionSort.java
+++ src/selectionSort.java
@@ -3,7 +3,7 @@ public class selectionSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//int m =-1;
+	
 		int arr1[] = {9,7,8,2,3,4,6};
 		for(int i=0;i<arr1.length-1;i++){
 			int m = i;
